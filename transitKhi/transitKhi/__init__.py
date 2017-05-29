ce9264f1da982e888b4aaf0fadc758c8300a5e78
Anusha Fatima <af00370@st.habib.edu.pk>
2017-05-29 20:16:00 +0500

    Create __init__.py

---
+++ transitKhi/transitKhi/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+# App config.
+DEBUG = True
+app = Flask(__name__, static_url_path = "/static", static_folder = "static")
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+import transitKhi.displayRoute, transitKhi.views
